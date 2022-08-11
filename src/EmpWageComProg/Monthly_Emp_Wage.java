024e9f32a845bea7a823ff6d5e5e513f0b76fb71
shreyas329 <sharmayashu1301@gmail.com>
2022-08-11 22:46:58 +0530

    Montly Wage of an employee is added.

---
+++ src/EmpWageComProg/Monthly_Emp_Wage.java
@@ -0,0 +1,35 @@
+package EmpWageComProg;
+
+public class Monthly_Emp_Wage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage programme.");
+		int FULL_TIME = 1;
+		int PART_TIME = 2;
+        int Wage_Per_Hr = 20;
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours = 0;
+        switch (empType){
+        	case 1:
+        		System.out.println("Full time Employee is Present");
+        		workingHours = 8;
+        		break;
+            
+        	case 2:
+        		System.out.println("Part time Employee is present");
+        		workingHours = 8;
+        		break;
+        	
+        	default:
+        		System.out.println("Employee is Absent");
+        	
+        }
+        int wage = (workingHours * Wage_Per_Hr);
+        System.out.println("Employee Daily Wage is: " + wage);
+        
+        int Total_Wage = (workingHours * Wage_Per_Hr) * 20;			//20 working days per month
+        System.out.println("Employee monthly total wage is: " + Total_Wage);
+
+	}
+
+}
