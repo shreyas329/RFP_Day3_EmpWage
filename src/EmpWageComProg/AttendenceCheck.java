127264c1b6bb51c62699d868216259763380348a
shreyas329 <sharmayashu1301@gmail.com>
2022-08-11 22:10:55 +0530

    Daily Employee Wage added

--- src/EmpWageComProg/AttendenceCheck.java
+++ src/EmpWageComProg/AttendenceCheck.java
@@ -3,6 +3,7 @@ package EmpWageComProg;
 public class AttendenceCheck {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage programme.");
 		int FULL_TIME = 1;
         int empType = (int) (Math.random() * 100 ) % 2;
         if (empType == FULL_TIME) {
---
+++ src/EmpWageComProg/Daily_Emp_Wage.java
@@ -0,0 +1,25 @@
+package EmpWageComProg;
+
+public class Daily_Emp_Wage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage programme.");
+		int FULL_TIME = 1;
+        int Wage_Per_Hr = 20;
+        int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+            workingHours = 8;
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+        int wage = workingHours * Wage_Per_Hr;
+        System.out.println("Employee Daily Wage is " + wage);
+
+	}
+
+}
